6d64c60d57ae1634e08cd08b25fed57bad16d8d5
lonzzi <lonzzi@qq.com>
2023-12-17 15:17:16 +0800

    adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import BMFooter from '@/components/BMFooter'
 
 function App() {
 	return (
-		<div className="app relative m-2 rounded-lg bg-[--bgColor] overflow-hidden">
-			<main className="flex h-[calc(100vh-(0.5rem)*2)] w-[calc(100vw-(0.5rem)*2)]">
-				<BMSider className="w-72 bg-slate-500" />
-				<BMMain className="flex-1 min-w-0" />
+		<div className="app relative m-2 rounded-lg bg-[--bgColor] border overflow-hidden">
+			<main className="flex h-[calc(100vh-(0.5rem+1px)*2)] w-[calc(100vw-(0.5rem+1px)*2)]">
+				<BMSider className="w-56 bg-[rgb(240,243,246)]" />
+				<BMMain className="flex-1 min-w-0 bg-[--bgColor]" />
 			</main>
 			<footer>
 				<BMFooter className="absolute bottom-0 h-[--footerHeight] backdrop-blur-3xl bg-opacity-80 border-t bg-white w-screen" />
