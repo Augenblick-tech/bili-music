301b6897f47494d7484dbcd14aa04d1f8e2ba6f3
lonzzi <lonzzi@qq.com>
2023-12-17 15:15:32 +0800

    adjust layout

--- src/components/BMFooter.tsx
+++ src/components/BMFooter.tsx
@@ -1,10 +1,61 @@
-import { FC } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { FCProps } from '@/types/FCProps'
+import { BiliVideoURL } from '@/types/bili/BiliVideo'
+import { getBiliVideoURL } from '@/api/BiliVideo'
 
 const BMFooter: FC<FCProps> = ({ className }) => {
+	const [videoData, setVideoData] = useState<BiliVideoURL>()
+
+	const videoRef = useRef<HTMLVideoElement>(null)
+	const [videoStat, setVideoStat] = useState<'playing' | 'paused'>('paused')
+
+	useEffect(() => {
+		getBiliVideoURL({
+			bvid: 'BV1y7411Q7Eq',
+			cid: 171776208,
+			fnval: 1,
+			platform: 'html5',
+		}).then((res) => {
+			console.log(res)
+			// console.log(res.data?.dash?.audio[0].baseUrl)
+			setVideoData(res.data)
+		})
+	}, [])
+
 	return (
-		<div className={className}>
-			<h1>BMFooter</h1>
+		<div className={`${className}`}>
+			<main className="flex justify-between relative">
+				<div className="song-info flex-1">
+					<video
+						className=" absolute bottom-0 right-0"
+						src={(() => {
+							const url = videoData?.durl[0].url
+							console.log(url)
+							return url
+						})()}
+						controls
+						ref={videoRef}
+					/>
+					<div className="name">BV1y7411Q7Eq</div>
+				</div>
+				<div className="control flex-1">
+					<button
+						onClick={() => {
+							if (videoStat === 'paused') {
+								videoRef.current?.play()
+								setVideoStat('playing')
+							} else {
+								videoRef.current?.pause()
+								setVideoStat('paused')
+							}
+						}}>
+						play
+					</button>
+				</div>
+				<div className="function flex-1">
+					<button>function</button>
+				</div>
+			</main>
 		</div>
 	)
 }
--- src/components/BMMain.tsx
+++ src/components/BMMain.tsx
@@ -6,7 +6,7 @@ import { Outlet } from 'react-router-dom'
 const BMMain: FC<FCProps> = ({ className }) => {
 	return (
 		<div className={className}>
-			<BMHeader className="h-[--headerHeight] bg-opacity-80 bg-blue-500" />
+			<BMHeader className="h-[--headerHeight]" />
 			<div className="bm-panel">
 				<Outlet />
 			</div>
