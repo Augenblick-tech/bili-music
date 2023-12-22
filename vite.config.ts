c86b5bdd0cf775f141c13af721a446a1edf05e44
Maxfield Wang <moposx01@gmail.com>
2023-12-22 11:03:09 +0800

    chore(test): add vitest
    This also involves a reformatting of the Vite config file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,73 +1,69 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
-import pkg from './package.json'
+/// <reference types="vitest" />
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "node:path"
+import electron from "vite-plugin-electron"
+import renderer from "vite-plugin-electron-renderer"
+import pkg from "./package.json"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-	const isServe = command === 'serve'
-	const isBuild = command === 'build'
-	const sourcemap = isServe || !!process.env.VSCODE_DEBUG
+  const isServe = command === "serve"
+  const isBuild = command === "build"
+  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
 
-	return {
-		plugins: [
-			react(),
-			electron([
-				{
-					// Main-Process entry file of the Electron App.
-					entry: 'electron/main/index.ts',
-					onstart(options) {
-						if (process.env.VSCODE_DEBUG) {
-							console.log(
-								/* For `.vscode/.debug.script.mjs` */ '[startup] Electron App'
-							)
-						} else {
-							options.startup()
-						}
-					},
-					vite: {
-						build: {
-							sourcemap,
-							minify: isBuild,
-							outDir: 'dist-electron/main',
-							rollupOptions: {
-								external: Object.keys(
-									'dependencies' in pkg ? pkg.dependencies : {}
-								),
-							},
-						},
-					},
-				},
-				{
-					entry: 'electron/preload/index.ts',
-					onstart(options) {
-						// Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
-						// instead of restarting the entire Electron App.
-						options.reload()
-					},
-					vite: {
-						build: {
-							sourcemap: sourcemap ? 'inline' : undefined, // #332
-							minify: isBuild,
-							outDir: 'dist-electron/preload',
-							rollupOptions: {
-								external: Object.keys(
-									'dependencies' in pkg ? pkg.dependencies : {}
-								),
-							},
-						},
-					},
-				},
-			]),
-			// Use Node.js API in the Renderer-process
-			renderer(),
-		],
-		resolve: {
-			alias: {
-				'@': path.join(__dirname, 'src'),
-			},
-		},
-	}
+  return {
+    test: {},
+    plugins: [
+      react(),
+      electron([
+        {
+          // Main-Process entry file of the Electron App.
+          entry: "electron/main/index.ts",
+          onstart(options) {
+            if (process.env.VSCODE_DEBUG) {
+              console.log(/* For `.vscode/.debug.script.mjs` */ "[startup] Electron App")
+            } else {
+              options.startup()
+            }
+          },
+          vite: {
+            build: {
+              sourcemap,
+              minify: isBuild,
+              outDir: "dist-electron/main",
+              rollupOptions: {
+                external: Object.keys("dependencies" in pkg ? pkg.dependencies : {}),
+              },
+            },
+          },
+        },
+        {
+          entry: "electron/preload/index.ts",
+          onstart(options) {
+            // Notify the Renderer-Process to reload the page when the Preload-Scripts build is complete,
+            // instead of restarting the entire Electron App.
+            options.reload()
+          },
+          vite: {
+            build: {
+              sourcemap: sourcemap ? "inline" : undefined, // #332
+              minify: isBuild,
+              outDir: "dist-electron/preload",
+              rollupOptions: {
+                external: Object.keys("dependencies" in pkg ? pkg.dependencies : {}),
+              },
+            },
+          },
+        },
+      ]),
+      // Use Node.js API in the Renderer-process
+      renderer(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.join(__dirname, "src"),
+      },
+    },
+  }
 })
